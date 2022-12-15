624a45ff662832fcaf4922950487ed7a0c438d67
LLarndorfer <116261839+LLarndorfer@users.noreply.github.com>
2022-12-15 09:53:01 +0100

    Add files via upload

---
+++ 021-finding-the-operator/Program.cs
@@ -0,0 +1,36 @@
+﻿//*Finding the operator
+//*Lukas Larndorfer
+//*01-12-2022
+string op = "";
+System.Console.WriteLine("Please enter the first Operand: ");
+float firstoperand = float.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Please enter the second Operand: ");
+float secondoperand = float.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Please enter the result: ");
+float result = float.Parse(Console.ReadLine()!);
+
+if (firstoperand * secondoperand == result)
+{
+    op = "Multiply";
+}
+if (firstoperand - secondoperand == result || secondoperand - firstoperand == result)
+{
+    op = "Minus";
+}
+if (firstoperand + secondoperand == result)
+{
+    op = "Plus";
+}
+if (firstoperand / secondoperand == result || secondoperand / firstoperand == result)
+{
+    op = "Divide";
+}
+if (Math.Pow(firstoperand, secondoperand) == result || Math.Pow(secondoperand, firstoperand) == result)
+{
+    op = "Exponentiation";
+}
+if (firstoperand % secondoperand == result || secondoperand % firstoperand == result)
+{
+    op = "Modulo";
+}
+System.Console.WriteLine("This is the possible operator for your number: " + op);
